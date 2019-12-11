d29c31ea1225e80085181603bb82f7ab39db9af8
Haris Sucipto <harisssucipto@gmail.com>
2019-12-11 08:11:31 +0700

    change to functional component

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,44 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class AddTodo extends React.Component {
-  constructor(props) {
-    super(props)
+export default function AddTodo({ addTodo }) {
 
-    this.state = {
-      input: ''
-    }
+  const [input, setInput] = useState('');
 
-    this.handleInput = this.handleInput.bind(this);
-    this.handleAdd = this.handleAdd.bind(this)
+  function handleInput(e) {
+    setInput(e.target.value)
   }
-
-  handleInput(e) {
-    this.setState({ input: e.target.value })
-  }
-
-  handleAdd() {
-    const { input } = this.state;
-    const { addTodo } = this.props;
-
+  function handleAdd() {
     if (input) {
       addTodo(input)
-      this.setState({ input: '' })
+      setInput('')
     }
   }
 
-  render() {
-    const { input } = this.state;
+  return (
+    <form onSubmit={
+      e => {
+        e.preventDefault();
+        handleAdd()
+      }
+    }>
+      <input type="text" placeholder="enter new task" style={{ width: 350, height: 15 }} value={input} onChange={handleInput} />
+      <input type="submit" style={{ float: 'right', marginTop: 2 }} value="add" disabled={!input} />
+    </form>
+  )
 
-    return (
-      <form onSubmit={
-        e => {
-          e.preventDefault();
-          this.handleAdd()
-        }
-      }>
-        <input type="text" placeholder="enter new task" style={{ width: 350, height: 15 }} value={input} onChange={this.handleInput} />
-        <input type="submit" style={{ float: 'right', marginTop: 2 }} value="add" disabled={!input} />
-      </form>
-    )
-  }
 }
