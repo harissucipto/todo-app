17ce6f143ed077d88c99b56f0563f38c41c29234
Haris Sucipto <harisssucipto@gmail.com>
2019-12-11 07:55:54 +0700

    making todo filter dynamic

--- src/TodoFilter.js
+++ src/TodoFilter.js
@@ -1,25 +1,40 @@
 import React from 'react';
 
+
 class TodoFilterItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleFilter = this.handleFilter.bind(this)
+  }
+
+  handleFilter() {
+    const { name, filterTodos } = this.props;
+    filterTodos(name)
+  }
+
   render() {
-    const { name } = this.props;
+    const { name, filter = 'all' } = this.props;
 
     const style = {
       color: 'blue',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      fontWeight: (filter === name) ? 'bold' : 'normal'
     }
 
-    return <span style={style}>{name}</span>
+    return <span style={style} onClick={this.handleFilter}>{name}</span>
   }
 }
 
 export default class TodoFilter extends React.Component {
+
   render() {
     return (
       <div>
-        <TodoFilterItem name="all" />{' / '}
-        <TodoFilterItem name="active" />{' / '}
-        <TodoFilterItem name="all" />{' / '}
+        <TodoFilterItem {...this.props} name="all" />{' / '}
+        <TodoFilterItem  {...this.props} name="active" />{' / '}
+        <TodoFilterItem  {...this.props} name="completed" />{' / '}
       </div>
     )
   }
